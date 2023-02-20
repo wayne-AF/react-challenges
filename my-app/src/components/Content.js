87f7823a192425c6c36329253c895a47c04f9338
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-02-20 15:00:13 +0000

    exercise 6 commit

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,12 +2,28 @@ import React, { Component } from 'react'
 import css from "./css/Content.module.css";
 import {savedPosts} from "../posts.json"
 import PostItem from "./PostItem"
+import Loader from './Loader'
 
 
 export class Content extends Component {
     constructor(props) {
       super(props)
+    
+      this.state = {
+        isLoaded: false,
+      }
     }
+
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          isLoaded: true,
+        })
+      }, 2000)
+    }
+
+
+
   render() {
     return (
       <div className={css.Content}>
@@ -16,7 +32,11 @@ export class Content extends Component {
         </div>
 
         <div className={css.SearchResults}>
-            <PostItem savedPosts={savedPosts} />
+          {
+          this.state.isLoaded ? <PostItem savedPosts={savedPosts} /> 
+          : <Loader />}
+            
+            
             
         </div>
       </div>
---
+++ my-app/src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import css from "./css/Loader.module.css";
+
+
+function Loader() {
+  return (
+    <div className={css.Loader}>
+
+    </div>
+  )
+}
+
+export default Loader
